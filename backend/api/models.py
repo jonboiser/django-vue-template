6275ba87175310996b98f90c3c7a723fc7a85fa4
Jonathan Boiser <jboiser@gmail.com>
2021-01-20 17:17:14 -0800

    Add Question model
    Add editorconfig

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,3 +11,35 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
         fields = ('url', 'subject', 'body', 'pk')
+
+
+class Question(models.Model):
+    ANSWER_CHOICES = (
+        (1, 'a'),
+        (2, 'b'),
+        (3, 'c'),
+        (4, 'd'),
+    )
+    prompt = models.CharField(max_length=200)
+    choice_a = models.CharField(max_length=200)
+    choice_b = models.CharField(max_length=200)
+    choice_c = models.CharField(max_length=200)
+    choice_d = models.CharField(max_length=200)
+    answer = models.IntegerField(choices=ANSWER_CHOICES)
+
+    def __str__(self):
+        return self.prompt
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    # TODO randomize choices on the backend
+    class Meta:
+        model = Question
+        fields = (
+            'prompt',
+            'choice_a',
+            'choice_b',
+            'choice_c',
+            'choice_d',
+            'answer'
+        )
